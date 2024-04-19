65fe25e2d9410cfdbeb2d245f9dfa20b909e70ca
tarak <tarakaram.gasi@gmail.com>
2024-04-19 12:50:53 -0500

    Implemented polymorphism Overload, override and virtual for parent child classes.

--- Child1/ChildClass1.cs
+++ Child1/ChildClass1.cs
@@ -1,7 +1,16 @@
-﻿namespace Child1
+﻿using Parent;
+namespace Child1
 {
-    public class ChildClass1
+    public class ChildClass1 : ParentClass
     {
+        public ChildClass1(string message):base(message)
+        {
+            Console.WriteLine("This is child class 1");
+        }
 
+        public override void Display(string message)
+        {
+            Console.WriteLine("This is Overrided display method");
+        }
     }
 }
