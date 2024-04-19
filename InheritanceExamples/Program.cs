6e25ee65f2c525c366f14d3411b54346e23e0e92
tarak <tarakaram.gasi@gmail.com>
2024-04-19 11:41:04 -0500

    Add project files.

---
+++ InheritanceExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InheritanceExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
